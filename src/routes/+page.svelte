2f4cfc45505c64f48075c165b8eda3c60afe4356
nekoniyah <beastish.maki@gmail.com>
2025-02-24 20:53:07 +0100

    e

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,3 +5,18 @@
 
 <Navbar />
 <Board />
+
+<style>
+	:global(*) {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+
+	:global(body) {
+		font-family:
+			-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+			'Helvetica Neue', sans-serif;
+		line-height: 1.6;
+	}
+</style>
