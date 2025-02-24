fb4d222362e80331464aa4c0d288cbe41f37cbb7
nekoniyah <beastish.maki@gmail.com>
2025-02-24 20:51:39 +0100

    e

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,7 @@
 <script>
 	import Board from '../components/game/Board.svelte';
 	import Navbar from '../components/common/Navbar.svelte';
-	let state = {};
 </script>
 
 <Navbar />
 <Board />
-
-<style lang="scss">
-</style>
